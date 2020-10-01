df7a71fe8c16e69b3629d1044a5f0d655663ad0c
Mattis <51265018+chaetognatha@users.noreply.github.com>
2020-10-01 17:31:42 +0200

    Update average.sh

--- average.sh
+++ average.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+#declare counter of number of args
 totalValues=0;
+#declare array holding numbers
 args=("$@");
+#loop through all args and add them together
 for (( i=0;i<$#;i++))
 do
 	totalValues=$((totalValues+${args[$i]}));
 done
+#calculate the average and print it all out
 echo "scale=1; $totalValues/$#" | bc;
